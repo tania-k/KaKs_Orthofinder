7660159bfb86a6b73910c52c4376a48a90497630
tania-k <tkurb001@ucr.edu>
2021-01-03 15:39:08 -0800

    Updated titles

--- Rscripts/Ks_plot.R
+++ Rscripts/Ks_plot.R
@@ -14,9 +14,9 @@ FsAll <- read_tsv("data/Friedmanniomyces_simplex_CCFEE_5184.KaKs.tsv")
 Fs2 <-FsAll %>% filter(dN <= 1)
 Fs <- FsAll %>% filter(dS <= 1)
 hist(Fs$dS,
-       main="Histogram of Friedmanniomyces simplex Ks",
-       xlab="Ks",
-       ylab="Count",
+       main="Ks Frequency of Friedmanniomyces simplex",
+       xlab="Ks Frequency",
+       ylab="Frequency Count",
        las=1,
        100)
 
@@ -25,9 +25,9 @@ Fe2=FeAll %>% filter(dN <= 1)
 Fe= FeAll %>% filter(dS <= 1)
 
 hist(Fe$dS,
-       main="Histogram of Friedmanniomyces endolithicus Ks",
-       xlab="Ks",
-       ylab="Count",
+       main="Ks Frequency of Friedmanniomyces endolithicus",
+       xlab="Ks Frequency",
+       ylab="Frequency Count",
        las=1,
        100)
 
@@ -35,9 +35,9 @@ HwAll=read_tsv("data/Hortaea_werneckii_EXF-2000-UCR.KaKs.tsv")
 Hw2=HwAll%>% filter(dN <=1)
 Hw=HwAll %>% filter(dS <=1)
 hist(Hw$dS,
-       main="Histogram of Hortaea werneckii Ks",
-       xlab="Ks",
-       ylab="Count",
+       main="Ks Frequency of Hortaea werneckii",
+       xlab="Ks Frequency",
+       ylab="Frequency Count",
        las=1,
        100)
 
@@ -46,14 +46,14 @@ dev.off()
 #I wanted to work with ggplot here to get nicer figures.
 KsPlotFile = "plots/Ks_histogram2.pdf"
 
-p1 <- ggplot(Fs,aes(x=dS)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ks Frequency Friedmanniomyces simplex", x="Ks", y="Count") +
+p1 <- ggplot(Fs,aes(x=dS)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ks Frequency of Friedmanniomyces simplex", x="Ks Frequency", y="Frequency Count") +
         scale_fill_gradient("Count", low="green", high="red") + theme_bw()
 
 
-p2 <- ggplot(Fe,aes(x=dS)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ks Frequency Friedmanniomyces endolithicus", x="Ks", y="Count") +
+p2 <- ggplot(Fe,aes(x=dS)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ks Frequency of Friedmanniomyces endolithicus", x="Ks Frequency", y="Frequency Count") +
         scale_fill_gradient("Count", low="green", high="red") + theme_bw()
 
-p3 <- ggplot(Hw,aes(x=dS)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ks Frequency Hortaea werneckii ", x="Ks", y="Count") +
+p3 <- ggplot(Hw,aes(x=dS)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ks Frequency of Hortaea werneckii", x="Ks Frequency", y="Frequency Count") +
         scale_fill_gradient("Count", low="green", high="red") + theme_bw()
 
 gridplot = plot_grid(p1, p2, p3, labels = c('A', 'B', 'C'), label_size = 12)
@@ -67,19 +67,19 @@ comboData <- bind_rows(
 
 KsPlotBarplot =  "plots/Ks_boxplot.pdf"
 p<- ggplot(comboData,aes(x=species,y=dS,color=species)) + geom_boxplot(outlier.shape = NA) + geom_jitter(size = 0.6,width=0.2,alpha=0.7,shape=16) + 
-        labs(title="Boxplot of Ks values in duplicated gene pairs", x="Species", y="Ks") + scale_color_brewer(palette='Set1') + theme_bw()
+        labs(title="Boxplot of Ks Frequency in Duplicated Gene Pairs Across Friedmanniomyces simplex, Friedmanniomyces endolithicus and Hortaea werneckii", x="Species", y="Ks Frequency") + scale_color_brewer(palette='Set1') + theme_bw()
 ggsave(KsPlotBarplot,p,width=12) 
 
 
 KaPlotFile = "plots/Ka_histogram.pdf"
-d1 <- ggplot(Fs2,aes(x=dN)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ka Frequency Friedmanniomyces simplex", x="Ka", y="Frequency") +
+d1 <- ggplot(Fs2,aes(x=dN)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ka Frequency of Friedmanniomyces simplex", x="Ka Frequency", y="Frequency Count") +
         scale_fill_gradient("Count", low="green", high="red") + theme_classic()
 
 
-d2 <- ggplot(Fe2,aes(x=dN)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ka Frequency Friedmanniomyces endolithicus", x="Ks", y="Frequency") +
+d2 <- ggplot(Fe2,aes(x=dN)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ka Frequency of Friedmanniomyces endolithicus", x="Ks Frequency", y="Frequency Count") +
         scale_fill_gradient("Count", low="green", high="red") + theme_classic()
 
-d3 <- ggplot(Hw2,aes(x=dN)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ka Frequency Hortaea werneckii ", x="Ka", y="Frequency") +
+d3 <- ggplot(Hw2,aes(x=dN)) + geom_histogram(binwidth=0.01,aes(fill=..count..), alpha = .8) + labs(title="Ka Frequency of Hortaea werneckii ", x="Ka Frequency", y="Frequency Count") +
         scale_fill_gradient("Count", low="green", high="red") + theme_classic()
 
 gridplot = plot_grid(d1, d2, d3, labels = c('A', 'B', 'C'), label_size = 12)
@@ -93,7 +93,7 @@ comboData <- bind_rows(
 
 KaPlotBarplot =  "plots/Ka_boxplot.pdf"
 d<- ggplot(comboData,aes(x=species,y=dN,color=species)) + geom_boxplot(outlier.shape = NA) + geom_jitter(size = 0.6,width=0.2,alpha=0.7,shape=16) + 
-        labs(title="Boxplot of Ka values in duplicated gene pairs", x="Species", y="Ka") + scale_color_brewer(palette='Set1')
+        labs(title="Boxplot of Ka Frequency in Duplicated Gene Pairs Across Friedmanniomyces simplex, Friedmanniomyces endolithicus and Hortaea werneckii", x="Species", y="Ka Frequency") + scale_color_brewer(palette='Set1')
 ggsave(KaPlotBarplot,d,width=12) 
 
 
@@ -106,7 +106,7 @@ comboData <- bind_rows(
 )
 
 p<- ggplot(comboData,aes(x=species,y=dS,color=species)) + geom_boxplot(outlier.shape = NA) + geom_jitter(size = 0.6,width=0.2,alpha=0.7,shape=16) + 
-        labs(title=sprintf("Boxplot of Ks values in duplicated gene pairs with Alignment < %d",MAXLEN), x="Species", y="Ks") + scale_color_brewer(palette='Set1') + theme_bw()
+        labs(title=sprintf("Boxplot of Ks Frequency in Duplicated Gene Pairs with Alignment < %d",MAXLEN), x="Species", y="Ks") + scale_color_brewer(palette='Set1') + theme_bw()
 ggsave(KsPlotBarplot,p,width=12) 
 p1 <- ggplot(subset(Fs,dS == 0),aes(x=LENGTH)) + geom_histogram(bins=30) + theme_bw() + labs(x="Alignment Length (bp)", title="Boxplot of Alignment Lengths for F. simplex where Ks = 0")
 p2 <- ggplot(subset(Fs,dS == 0 & LENGTH < 80),aes(x=LENGTH)) + geom_histogram(bins=30) + theme_bw() + labs(x="Alignment Length (bp)", title="Boxplot of Alignment Lengths for F. simplex where Ks = 0 & LENGTH < 80")
